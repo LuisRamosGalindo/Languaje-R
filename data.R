4a44021cb67978fa1b689f31e60802329309eabc
Luís Ramos Galindo <38390112+LuisRamosGalindo@users.noreply.github.com>
2019-07-25 02:40:08 -0500

    Add files via upload

---
+++ data.R
@@ -0,0 +1,6 @@
+x <-
+"Meta data"
+y <-
+structure(list(a = c(1, 2), b = structure(1:2, .Label = c("a", 
+"b"), class = "factor")), .Names = c("a", "b"), row.names = c(NA, 
+-2L), class = "data.frame")
