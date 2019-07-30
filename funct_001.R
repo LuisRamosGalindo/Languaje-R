5b00733743fae2128603e1f7233e84de05ac6d7e
Luís Ramos Galindo <38390112+LuisRamosGalindo@users.noreply.github.com>
2019-07-30 00:39:27 -0500

    Add files via upload

---
+++ funct_001.R
@@ -0,0 +1,21 @@
+## ################################################## 
+## #   Función artimetica                             #
+## ##################################################
+arith<- function(x,y){
+  result <- x*y
+  print(paste("Resultado : ", toString(result), SEP=" ", collapse= NULL ));
+}
+## ################################################## 
+## #  Promedia de una secuencia de números          #
+## ##################################################
+promedio <- function (x){
+  
+  suma <- 0
+  for(i in x){
+    suma<-suma + i
+  }
+  
+  suma<- suma/length(x)
+  print(suma)
+}
+ 
