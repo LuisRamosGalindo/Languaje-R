c0a0997885b51ff4ed0cac8e5c9bb2c0c3978bd7
Luís Ramos Galindo <38390112+LuisRamosGalindo@users.noreply.github.com>
2019-07-30 01:33:48 -0500

    Add files via upload
    cuarta actualización

--- funct_001.R
+++ funct_001.R
@@ -1,12 +1,12 @@
 ## ################################################## 
-## #   Función artimetica                             #
+## #   Función artimetica. 1                             #
 ## ##################################################
 arith<- function(x,y){
   result <- x*y
   print(paste("Resultado : ", toString(result), SEP=" ", collapse= NULL ));
 }
 ## ################################################## 
-## #  Promedio de una secuencia de números.          #
+## #  Promedio de una secuencia de números.  2        #
 ## ##################################################
 promedio <- function (x){
   
